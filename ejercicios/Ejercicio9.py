8e4d16f3d58c309cd6861197eabaee5d6bc2a911
javimiguelez <91721699+Xavitheforce@users.noreply.github.com>
2022-02-23 12:29:57 +0000

    .

---
+++ ejercicios/Ejercicio9.py
@@ -0,0 +1,57 @@
+palabras={
+  1:{
+    'palabra': 'avion',
+    'siguiente': 2,
+    'anterior': 8
+  },
+  2:{
+    'palabra': 'coche',
+    'siguiente': 10,
+    'anterior': 1
+  },
+  3:{
+    'palabra': 'gorro',
+    'siguiente': 7,
+    'anterior': 10
+  },
+  4:{
+    'palabra': 'pan',
+    'siguiente': 9,
+    'anterior': 6
+  },
+  5:{
+    'palabra': 'iglesia',
+    'siguiente': 1,
+    'anterior': 2
+  },
+  6:{
+    'palabra': 'pagina',
+    'siguiente': 4,
+    'anterior': 7
+  },
+  7:{
+    'palabra': 'orar',
+    'siguiente': 6,
+    'anterior': 3
+  },
+  8:{
+    'palabra': 'anaconda',
+    'siguiente': 1,
+    'anterior': 0
+  },
+  9:{
+    'palabra': 'programar',
+    'siguiente': 11,
+    'anterior': 4
+  },
+  10:{
+    'palabra': 'cuaderno',
+    'siguiente': 3,
+    'anterior': 2
+  },
+  11:{
+    'palabra': 'zapato',
+    'siguiente': 12,
+    'anterior': 9
+  },
+}
